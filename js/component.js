b9a20127dabc9e0797b2c73ab6f1030582549e10
Pobporn <pobpornbu@gmail.com>
2015-06-03 15:44:08 -0700

    [html/css] radio player

---
+++ js/component.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	// Textarea
+	autosize($('textarea'));
+	$('.js-comment-textarea').on('focus', function() {
+		$(this).parent().toggleClass('active');
+	});
+
+  $(window).load(function(){   
+    $("#previous-clip").mCustomScrollbar({
+      theme:"minimal"
+    });
+  });
+
+})(jQuery);
